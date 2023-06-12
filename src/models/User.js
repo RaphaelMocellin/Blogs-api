67dbfa80bdc4b1af82e5b6a1706beb90ce1e8af7
rapha <raphael.mocellin@gmail.com>
2023-06-12 16:42:57 -0300

    Elaborado o req 2

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+    const Book = sequelize.define('User', {
+      id: DataTypes.INTEGER,
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    });
+  
+    return Book;
+  };
