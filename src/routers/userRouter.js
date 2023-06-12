45a9fd7f84731561d35cb84c0e244076deccbd3b
rapha <raphael.mocellin@gmail.com>
2023-06-12 19:23:21 -0300

    Elaborado o midd de auth e o req 5

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,9 +5,11 @@ const { userController } = require('../controllers');
 const validateDisplayName = require('../middlewares/validateDisplayName');
 const validateEmail = require('../middlewares/validateEmail');
 const validatePassword = require('../middlewares/validatePassword');
+const tokenAuth = require('../middlewares/auth');
 
 const userRouter = Router();
 
+userRouter.get('/', tokenAuth, userController.getAll);
 userRouter.post(
 '/', 
 validateDisplayName,
--- src/services/user.services.js
+++ src/services/user.services.js
@@ -3,17 +3,12 @@ const { User } = require('../models');
 
 const secret = process.env.JWT_SECRET || 'seusecretdetoken';
 
-/* Esta função usa o método findAll do Sequelize para buscar todas as linhas da tabela Users
-Equivale a fazer a query: SELECT * FROM Users */
 const getAll = async () => {
-  const users = await User.findAll();
+  const users = await User.findAll({ attributes: { exclude: ['password'] } });
 
   return users;
 };
 
-/* Esta função usa o método findOne do Sequelize combinado 
-com a chave where para buscar por email e password. 
-Equivale a fazer a query: SELECT * FROM Users WHERE id=? AND email=? */
 const getByEmailAndPassword = async (email, password) => {
     const user = await User.findOne({ where: { email, password } });
   
